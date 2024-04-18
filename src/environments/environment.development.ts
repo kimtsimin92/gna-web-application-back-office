251c8b9de625c86c091937c557e5b2ea91526f1f
Kimt Simin <kimt.simin@ngser.com>
2024-04-18 08:54:59 +0000

    fix app

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
-  apiGateway: "http://localhost:28080",
+/*  apiGateway: "http://localhost:28080",
   usersService: "http://localhost:28080/users-service",
-  productsService: "http://localhost:28080/products-service",
-/*  apiGateway: "http://159.8.251.21:28080",
+  productsService: "http://localhost:28080/products-service",*/
+  apiGateway: "http://159.8.251.21:28080",
   usersService: "http://159.8.251.21:28080/users-service",
-  productsService: "http://159.8.251.21:28080/products-service",*/
+  productsService: "http://159.8.251.21:28080/products-service",
 };
