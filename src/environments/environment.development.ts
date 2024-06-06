3d0b28751b298d923ec00c5983002b6c33e74e23
Kimt Simin <kimt.simin@ngser.com>
2024-06-06 04:39:17 +0000

    fix app

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   apiGateway: "http://localhost:28080",
   usersService: "http://localhost:28080/users-service",
   productsService: "http://localhost:28080/products-service",
-  customersService: "http://160.120.120.18:28090"
+  customersService: "http://crossroadtest.net:28090"
 };
 
 
